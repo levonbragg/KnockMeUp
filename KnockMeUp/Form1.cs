b697fd0383cb4a1b5185bda108051a055b0ab0b7
Levon Bragg <levonbragg@gmail.com>
2022-07-31 21:24:10 -0700

    working test!

--- KnockMeUp/Form1.cs
+++ KnockMeUp/Form1.cs
@@ -7,25 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace KnockMeUp
 {
     public partial class Server
     {
-        public string host { get; set; }=string.Empty;
-        public string description { get; set; }=string.Empty;
-        public int packet1type { get; set; } = 0;
-        public int packet1port { get; set; } = 0;
-        public string packet1text { get; set; } = string.Empty;
-        public int packet2type { get; set; } = 0;
-        public int packet2port { get; set; } = 0;
-        public string packet2text { get; set; } = string.Empty;
-        public int packet3type { get; set; } = 0;
-        public int packet3port { get; set; } = 0;
-        public string packet3text { get; set; } = string.Empty;
-        public int packet4type { get; set; } = 0;
-        public int packet4port { get; set; } = 0;
-        public string packet4text { get; set; } = string.Empty;
+        public string Host { get; set; }=string.Empty;
+        public string Description { get; set; }=string.Empty;
+        public int Packet1type { get; set; } = 0;
+        public int Packet1port { get; set; } = 0;
+        public string Packet1text { get; set; } = string.Empty;
+        public int Packet2type { get; set; } = 0;
+        public int Packet2port { get; set; } = 0;
+        public string Packet2text { get; set; } = string.Empty;
+        public int Packet3type { get; set; } = 0;
+        public int Packet3port { get; set; } = 0;
+        public string Packet3text { get; set; } = string.Empty;
+        public int Packet4type { get; set; } = 0;
+        public int Packet4port { get; set; } = 0;
+        public string Packet4text { get; set; } = string.Empty;
 
 
     }
@@ -38,11 +39,53 @@ namespace KnockMeUp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void btnKnock_Click(object sender, EventArgs e)
         {
+            // create a new object for testing...
+
+            Server server = new Server();
+            server.Host = "172.31.251.254";
+            server.Description = "Home F12";
+            server.Packet1type = 1;
+            server.Packet1port = 38617;
+            server.Packet1text = "Gd57xb1zNlQvedtUuP14qa3eWka618TjLhVvvTCW==";
+            server.Packet2type = 1;
+            server.Packet2port = 10994;
+            server.Packet2text = "Gd57xb1zNlQvedtUuP14qa3eWka618TjLhVvvTCW==";
+            server.Packet3type = 1;
+            server.Packet3port = 53571;
+            server.Packet3text = "Gd57xb1zNlQvedtUuP14qa3eWka618TjLhVvvTCW==";
+            server.Packet4type = 1;
+            server.Packet4port = 27589;
+            server.Packet4text = "Gd57xb1zNlQvedtUuP14qa3eWka618TjLhVvvTCW==";
+
+            knock(server);
+
+        }
+
+        private void knock (Server server)
+        {
+            if (server is null)
+            {
+                throw new ArgumentNullException(nameof(server));
+            }
+
+            // send UDP test
+            UdpClient udpClient = new UdpClient();
+
+            Byte[] sendBytes = Encoding.ASCII.GetBytes(server.Packet1text);
+            try
+            {
+                udpClient.Send(sendBytes, sendBytes.Length, server.Host, server.Packet1port);
+            }
+            catch (Exception err)
+            {
+                //Console.WriteLine(e.ToString());
+            }
+
 
         }
     }
